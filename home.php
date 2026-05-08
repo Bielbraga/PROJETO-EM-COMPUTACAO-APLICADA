be51e3e8be67fc261da2ceeca65cfd321d254b92
Bielbraga <gabriel.s.braga99@gmail.com>
2026-05-08 17:33:58 -0300

    feat(dashboard): implement static dashboard layout with sidebar navigation

--- home.php
+++ home.php
@@ -9,12 +9,19 @@ include("includes/auth.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FocusFlow - Dashboard</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet"
+    <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    rel="stylesheet">
+
+    <link
+    rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
 
@@ -43,7 +50,6 @@ include("includes/auth.php");
                 </div>
 
                 <div class="task-section">
-
                     <h3>Tarefas de Hoje</h3>
 
                     <div class="task-card">
@@ -69,7 +75,6 @@ include("includes/auth.php");
                             <p>Push day às 19h.</p>
                         </div>
                     </div>
-
                 </div>
 
                 <div class="add-task-box">
@@ -85,7 +90,6 @@ include("includes/auth.php");
 
                 <div class="info-card">
                     <h4>Sugestões</h4>
-
                     <ul>
                         <li>📘 Revisar matéria</li>
                         <li>💻 Finalizar projeto</li>
@@ -95,7 +99,6 @@ include("includes/auth.php");
 
                 <div class="info-card">
                     <h4>Resumo</h4>
-
                     <div class="stats">
                         <div>
                             <strong>12</strong>
@@ -111,7 +114,6 @@ include("includes/auth.php");
 
                 <div class="info-card">
                     <h4>Calendário</h4>
-
                     <div class="calendar-fake">
                         <span>Seg</span>
                         <span>Ter</span>
