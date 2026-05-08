76a87fab3372c2dd43a4411603e74ff25b93809b
Bielbraga <gabriel.s.braga99@gmail.com>
2026-05-08 17:26:35 -0300

    feat(dashboard): create static dashboard interface

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,3 +1,7 @@
+<?php
+$paginaAtual = basename($_SERVER['PHP_SELF']);
+?>
+
 <div class="sidebar">
 
     <div class="logo-area">
@@ -9,32 +13,32 @@
 
     <nav class="menu">
 
-        <a href="home.php" class="menu-item active">
+        <a href="home.php" class="menu-item <?= ($paginaAtual == 'home.php') ? 'active' : '' ?>">
             <i class="fa-solid fa-house"></i>
             <span>Meu Dia</span>
         </a>
 
-        <a href="tarefas.php" class="menu-item">
+        <a href="tarefas.php" class="menu-item <?= ($paginaAtual == 'tarefas.php') ? 'active' : '' ?>">
             <i class="fa-solid fa-list-check"></i>
             <span>Tarefas</span>
         </a>
 
-        <a href="importante.php" class="menu-item">
+        <a href="importante.php" class="menu-item <?= ($paginaAtual == 'importante.php') ? 'active' : '' ?>">
             <i class="fa-solid fa-star"></i>
             <span>Importante</span>
         </a>
 
-        <a href="concluidas.php" class="menu-item">
+        <a href="concluidas.php" class="menu-item <?= ($paginaAtual == 'concluidas.php') ? 'active' : '' ?>">
             <i class="fa-solid fa-circle-check"></i>
             <span>Concluídas</span>
         </a>
 
-        <a href="pessoal.php" class="menu-item">
+        <a href="pessoal.php" class="menu-item <?= ($paginaAtual == 'pessoal.php') ? 'active' : '' ?>">
             <i class="fa-solid fa-user"></i>
             <span>Pessoal</span>
         </a>
 
-        <a href="faculdade.php" class="menu-item">
+        <a href="faculdade.php" class="menu-item <?= ($paginaAtual == 'faculdade.php') ? 'active' : '' ?>">
             <i class="fa-solid fa-graduation-cap"></i>
             <span>Faculdade</span>
         </a>
